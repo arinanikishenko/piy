1fcf1f2094bf10460de7a36c8c7517e16416ab76
arinanikishenkoGITHUB <steklovaesenya@yandex.ru>
2019-05-15 17:11:29 +0300

    pik commit

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ public class Main {
         System.out.println("Hello World!");
         System.out.println("The storm darkens the sky!");
         System.out.println("Snow twisting swirls");
-<<<<<<< HEAD
         System.out.println("That will cry like a child");
-=======
         System.out.println("That how the beast she howls");
->>>>>>> pi
     }
 }
